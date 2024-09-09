7449f703209b874c9534dfa179407ab83e1cb79f
twonezero <dnjsdudtm98@naver.com>
2024-09-09 13:57:01 +0900

    feat: Company CRUD, Search API 기본 구현
    - 추후 검증이 필요한 로직은 논의 후 리팩토링

--- company/src/main/java/com/sparta/ch4/delivery/company/domain/service/CompanyDomainService.java
+++ company/src/main/java/com/sparta/ch4/delivery/company/domain/service/CompanyDomainService.java
@@ -2,14 +2,19 @@ package com.sparta.ch4.delivery.company.domain.service;
 
 
 import com.sparta.ch4.delivery.company.application.dto.CompanyDto;
+import com.sparta.ch4.delivery.company.domain.model.Company;
 import com.sparta.ch4.delivery.company.domain.repository.CompanyRepository;
+import com.sparta.ch4.delivery.company.domain.type.CompanySearchType;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+//TODO : 에러 내용 정의
 @RequiredArgsConstructor
 @Service
 public class CompanyDomainService {
@@ -20,10 +25,39 @@ public class CompanyDomainService {
         return CompanyDto.from(companyRepository.save(dto.toEntity()));
     }
 
-    //TODO : 에러 내용 정의
+
     public CompanyDto getCompanyById(UUID id) {
         return companyRepository.findById(id).map(CompanyDto::from).orElseThrow(
                 () -> new EntityNotFoundException("ID 에 해당하는 업체가 존재하지 않습니다.")
         );
+    }
+
+    public Page<CompanyDto> getAllCompanies(CompanySearchType searchType, String searchValue, Pageable pageable) {
+        return companyRepository.searchCompany(searchType, searchValue, pageable).map(CompanyDto::from);
+    }
+
+    public CompanyDto updateCompany(UUID companyId, CompanyDto dto) {
+        // company 조회
+        Company company = companyRepository.findById(companyId).orElseThrow(
+                () -> new EntityNotFoundException("ID 에 해당하는 업체가 존재하지 않습니다.")
+        );
+        // 업데이트
+        company.setHubId(dto.hubId());
+        company.setName(dto.name());
+        company.setType(dto.type());
+        company.setAddress(dto.address());
+        // 저장
+        return CompanyDto.from(companyRepository.save(company));
+    }
+
+    public void deleteCompany(UUID companyId, String userId) {
+        // company 조회
+        Company company = companyRepository.findById(companyId).orElseThrow(
+                () -> new EntityNotFoundException("ID 에 해당하는 업체가 존재하지 않습니다.")
+        );
+        // Soft delete
+        company.setIsDeleted(true);
+        company.setDeletedAt(LocalDateTime.now());
+        company.setDeletedBy(userId);
     }
 }
---
+++ company/src/main/java/com/sparta/ch4/delivery/company/presentation/request/CompanyUpdateRequest.java
@@ -0,0 +1,30 @@
+package com.sparta.ch4.delivery.company.presentation.request;
+
+import com.sparta.ch4.delivery.company.application.dto.CompanyDto;
+import com.sparta.ch4.delivery.company.domain.type.CompanyType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record CompanyUpdateRequest(
+        @NotNull(message = "허브 ID 는 비어있을 수 없습니다.")
+        UUID hubId,
+        @NotBlank(message = "업체 이름은 비어있을 수 없습니다.")
+        String name,
+        @NotBlank(message = "업체 타입은 비어있을 수 없습니다.")
+        CompanyType type,
+        @NotBlank(message = "업체 주소는 비어있을 수 없습니다.")
+        String address
+) {
+
+    public CompanyDto toDto(String userId) {
+        return CompanyDto.builder()
+                .hubId(hubId)
+                .name(name)
+                .type(type)
+                .address(address)
+                .updatedBy(userId) //수정자
+                .build();
+    }
+}
