43be0c2f8703a4c87fbe3885ef8d0a29f390d66c
Bae Ji Won <qowhxk@naver.com>
2024-09-09 12:28:59 +0900

    feat: hub server 기본 설정

--- hub/src/main/java/com/example/hub/HubApplication.java
+++ hub/src/main/java/com/example/hub/HubApplication.java
@@ -2,8 +2,11 @@ package com.example.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HubApplication {
 
 	public static void main(String[] args) {
---
+++ hub/src/main/java/com/example/hub/domain/model/BaseEntity.java
@@ -0,0 +1,52 @@
+package com.example.hub.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Setter
+    @Column(name = "created_by", length = 100)
+    private String createdBy;
+
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Setter
+    @Column(name = "updated_by", length = 100)
+    private String updatedBy;
+
+    @Setter
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Setter
+    @Column(name = "deleted_by", length = 100)
+    private String deletedBy;
+
+    @Setter
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted;
+
+    public BaseEntity(){
+        this.isDeleted = false;
+    }
+
+}
