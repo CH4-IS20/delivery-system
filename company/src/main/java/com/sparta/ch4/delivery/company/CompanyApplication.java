58873a5a8ecd3d7f36014ce078db3588a3fe5ff9
twonezero <dnjsdudtm98@naver.com>
2024-09-09 13:55:53 +0900

    feat: Paging 데이터 직렬화 경고 없애는 설정 추가
    어플리케이션에 `@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)` 추가

--- company/src/main/java/com/sparta/ch4/delivery/company/CompanyApplication.java
+++ company/src/main/java/com/sparta/ch4/delivery/company/CompanyApplication.java
@@ -3,9 +3,11 @@ package com.sparta.ch4.delivery.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class CompanyApplication {
 
 	public static void main(String[] args) {
