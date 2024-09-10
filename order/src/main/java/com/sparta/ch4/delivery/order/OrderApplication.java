e59cfb03e3ea6838bc56466429749ed6c9c96a70
twonezero <dnjsdudtm98@naver.com>
2024-09-10 11:42:27 +0900

    build: 주문, 배송, 배송 기록을 위한 프로젝트 생성 및 설정

---
+++ order/src/main/java/com/sparta/ch4/delivery/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ch4.delivery.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
