2d8a06149c0424c5c02b934d223b26f767fb0632
twonezero <dnjsdudtm98@naver.com>
2024-09-10 14:03:07 +0900

    feat: Jpa query 설정 bean , Paging 직렬화 관련 설정

--- order/src/main/java/com/sparta/ch4/delivery/order/OrderApplication.java
+++ order/src/main/java/com/sparta/ch4/delivery/order/OrderApplication.java
@@ -2,8 +2,12 @@ package com.sparta.ch4.delivery.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@FeignClient
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class OrderApplication {
 
 	public static void main(String[] args) {
---
+++ order/src/main/java/com/sparta/ch4/delivery/order/infrastructure/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.sparta.ch4.delivery.order.infrastructure.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManagerFactory entityManagerFactory) {
+        return new JPAQueryFactory(entityManagerFactory.createEntityManager());
+    }
+}
