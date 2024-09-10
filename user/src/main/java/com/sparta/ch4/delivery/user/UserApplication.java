a74fbd411dbacdf7c2a0b91d03830a2003920b60
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-09-10 11:31:25 +0900

    feat: Page Dto 직렬화 설정 추가

--- user/src/main/java/com/sparta/ch4/delivery/user/UserApplication.java
+++ user/src/main/java/com/sparta/ch4/delivery/user/UserApplication.java
@@ -3,9 +3,13 @@ package com.sparta.ch4.delivery.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSpringDataWebSupport(
+		pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 public class UserApplication {
 
 	public static void main(String[] args) {
