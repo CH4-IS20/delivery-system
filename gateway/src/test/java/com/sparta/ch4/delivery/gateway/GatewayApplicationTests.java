858eb746daead9e1cc59e94dc267e48356e8f712
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-09-11 14:10:53 +0900

    build: gateway 초기 설정 추가

---
+++ gateway/src/test/java/com/sparta/ch4/delivery/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ch4.delivery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
