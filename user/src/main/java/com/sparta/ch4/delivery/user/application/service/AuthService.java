e8cdbc9e8fa860e7292a2d96dfda41d527c68c25
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-09-13 18:32:57 +0900

    feat: ErrorCode 및 Exception 핸들러 구현

--- user/src/main/java/com/sparta/ch4/delivery/user/application/service/AuthService.java
+++ user/src/main/java/com/sparta/ch4/delivery/user/application/service/AuthService.java
@@ -2,12 +2,13 @@ package com.sparta.ch4.delivery.user.application.service;
 
 import com.sparta.ch4.delivery.user.application.dto.LoginDto;
 import com.sparta.ch4.delivery.user.application.dto.RegisterDto;
+import com.sparta.ch4.delivery.user.domain.exception.ApplicationException;
+import com.sparta.ch4.delivery.user.domain.exception.ErrorCode;
 import com.sparta.ch4.delivery.user.domain.model.User;
 import com.sparta.ch4.delivery.user.domain.repository.UserRepository;
 import com.sparta.ch4.delivery.user.domain.service.UserDomainService;
 import com.sparta.ch4.delivery.user.infrastructure.jwt.JwtTokenProvider;
 import com.sparta.ch4.delivery.user.presentation.response.UserResponse;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,7 +35,7 @@ public class AuthService {
     @Transactional(readOnly = true)
     public UserResponse login(HttpServletResponse response, LoginDto dto) {
         User user = userRepository.findByUsername(dto.username())
-                .orElseThrow(() -> new EntityNotFoundException("Username 에 해당하는 사용자가 없습니다."));
+                .orElseThrow(() -> new ApplicationException(ErrorCode.USERNAME_NOT_FOUND));
         checkPassword(dto.password(), user.getPassword());
 
         String token = jwtTokenProvider.createToken(user.getId(), user.getUsername(), user.getRole());
@@ -45,7 +46,7 @@ public class AuthService {
 
     private void checkPassword(String inputPassword, String userPassword) {
         if (!passwordEncoder.matches(inputPassword, userPassword)) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            throw new ApplicationException(ErrorCode.PASSWORD_MISMATCH);
         }
     }
 }
