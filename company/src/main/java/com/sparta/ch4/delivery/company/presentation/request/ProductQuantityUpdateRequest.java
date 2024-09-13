a68d3aba23879620510ac7ccf4964acb2176731f
twonezero <dnjsdudtm98@naver.com>
2024-09-13 21:01:06 +0900

    fix: Product 수량 변경 요청 객체 validation 추가

--- company/src/main/java/com/sparta/ch4/delivery/company/presentation/request/ProductQuantityUpdateRequest.java
+++ company/src/main/java/com/sparta/ch4/delivery/company/presentation/request/ProductQuantityUpdateRequest.java
@@ -2,10 +2,13 @@ package com.sparta.ch4.delivery.company.presentation.request;
 
 import com.sparta.ch4.delivery.company.domain.type.ProductQuantity;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
 @Builder
 public record ProductQuantityUpdateRequest(
+        @Min(value = 0)
+        @Schema(description = "추가하거나 차감할 상품 수량")
         Integer quantity,
         @Schema(description = "수량 추가/차감 (Enum)", enumAsRef = true)
         ProductQuantity upAndDown
