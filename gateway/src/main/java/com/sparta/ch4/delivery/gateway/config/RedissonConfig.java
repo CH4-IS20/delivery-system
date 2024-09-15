939d6df0c1d2ce3913514ab88fdee78bfcb328b6
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-09-15 13:27:20 +0900

    build: 프로필 별 redis 환경 변수 설정

--- gateway/src/main/java/com/sparta/ch4/delivery/gateway/config/RedissonConfig.java
+++ gateway/src/main/java/com/sparta/ch4/delivery/gateway/config/RedissonConfig.java
@@ -4,16 +4,20 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.KryoCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${redisson.url}")
+    private String redissonUrl;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        config.useSingleServer().setAddress(redissonUrl);
         config.setCodec(new KryoCodec());
         return Redisson.create(config);
     }
