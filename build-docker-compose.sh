02116eec732eae2f92cf34ef9db4b149c0493410
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-09-16 15:20:19 +0900

    build: docker compose 쉘 스크립트 수정

--- build-docker-compose.sh
+++ build-docker-compose.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# 네트워크 생성
+echo "Creating networks..."
+docker network create external-network
+docker network create internal-network --internal
+docker network create monitoring-network
+
 COMPOSE_DIR="./docker"
 
 COMPOSE_FILES=(
-    "docker-compose.network.yml"
+    "docker-compose.eureka.yml"
     "docker-compose.cache.yml"
     "docker-compose.company.yml"
     "docker-compose.order.yml"
     "docker-compose.user.yml"
-    "docker-compose.gateway.yml"
-    "docker-compose.eureka.yml"
     "docker-compose.hub.yml"
+    "docker-compose.gateway.yml"
 )
 
+# Docker Compose 파일을 순차적으로 빌드하고 실행
 for compose_file in "${COMPOSE_FILES[@]}"; do
     echo "Building and starting services from $compose_file"
     docker-compose -f "$COMPOSE_DIR/$compose_file" up --build -d
