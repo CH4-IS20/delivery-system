65afb6e4427442c1e10f09315fdc4896386862a9
twonezero <dnjsdudtm98@naver.com>
2024-09-19 10:26:11 +0900

    feat: ExceptionHandler validation 관련 에러 핸들러 추가

--- company/src/main/java/com/sparta/ch4/delivery/company/domain/exception/ExceptionManager.java
+++ company/src/main/java/com/sparta/ch4/delivery/company/domain/exception/ExceptionManager.java
@@ -1,7 +1,9 @@
 package com.sparta.ch4.delivery.company.domain.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,10 +13,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionManager {
 
     @ExceptionHandler(ApplicationException.class)
-    public ResponseEntity<ErrorResponse<?>> applicationExceptionHandler(ApplicationException e) {
+    public ResponseEntity<ErrorResponse> applicationExceptionHandler(ApplicationException e) {
         log.error(e.getMessage());
         return ResponseEntity
                 .status(e.getErrorCode().getStatus())
                 .body(ErrorResponse.error(e.getErrorCode()));
+    }
+
+    // @Valid 검증에 대한 에러 출력
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> requestMethodNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(e.getMessage())
+                .build();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errorResponse.addValidation(error.getField(), error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
